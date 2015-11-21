d7c2f4213758ffd1a55ad562054df3d4dba72cb5
Paul Batchelor <thisispaulbatchelor@gmail.com>
2015-11-20 18:25:32 -0800

    noteSort now parses strings

--- golang/noteSort.go
+++ golang/noteSort.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"os"
 	"sort"
+	"strconv"
+	"strings"
 )
 
 type Note struct {
@@ -53,13 +55,33 @@ func WriteNotes(file *os.File, notes *[]Note) {
 	}
 }
 
-func main() {
-	notes := []Note{
-		{0, []float32{5.0, 60, 0.1}},
-		{1, []float32{4.0, 62, 0.1}},
-		{2, []float32{3.0, 71, 0.1}},
-		{3, []float32{2.0, 64, 0.1}},
+func StrToNote(str string) Note {
+	var nt Note
+	vals := strings.Split(str, " ")
+	start, _ := strconv.ParseFloat(vals[0], 32)
+	nt.Start = float32(start)
+	for _, k := range vals[1:] {
+		flt, err := strconv.ParseFloat(k, 32)
+		if err != nil {
+			fmt.Println("Error: something happened with the parser...")
+		}
+		nt.Vals = append(nt.Vals, float32(flt))
 	}
+	return nt
+}
+
+func main() {
+	//notes := []Note{
+	//	{0, []float32{5.0, 60, 0.1}},
+	//	{1, []float32{4.0, 62, 0.1}},
+	//	{2, []float32{3.0, 71, 0.1}},
+	//	{3, []float32{2.0, 64, 0.1}},
+	//}
+	var notes []Note
+	notes = append(notes, StrToNote("0 5.0 60 0.1"))
+	notes = append(notes, StrToNote("1 4.0 62 0.1"))
+	notes = append(notes, StrToNote("2 3.0 71 0.1"))
+	notes = append(notes, StrToNote("3 2.0 64 0.1"))
 	fmt.Println(notes)
 	file, _ := os.Create("test.bin")
 	defer file.Close()
